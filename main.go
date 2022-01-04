10049ab59b752f7effbd97fba51cb5f40b7c708b
Gustavo Caldeira <caldeira.santos@gmail.com>
2022-01-04 11:26:14 -0300

    Fix region execution

--- main.go
+++ main.go
@@ -13,29 +13,3 @@ func main() {
 	cmd.AssetData = assetData
 	cmd.Execute()
 }
-
-// func NewCommand() *cobra.Command {
-// 	var slice []string
-// 	var arr []string
-// 	c := &cobra.Command{
-// 		Use: "cmd",
-// 		Run: func(cmd *cobra.Command, args []string) {
-// 			for _, v := range slice {
-// 				fmt.Println(v)
-// 			}
-
-// 			for _, v := range arr {
-// 				fmt.Println(v)
-// 			}
-// 		},
-// 	}
-
-// 	c.Flags().StringSliceVarP(&slice, "slice", "s", []string{}, "")
-// 	c.Flags().StringArrayVarP(&arr, "arr", "a", []string{}, "")
-
-// 	return c
-// }
-
-// func main() {
-// 	NewCommand().Execute()
-// }
